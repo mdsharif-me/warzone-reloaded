cd9b078ddf77e822d1d7286b6bbbdf1ee201c4ae
deeppatel-11 <60020321+deeppatel-11@users.noreply.github.com>
2022-02-08 15:42:27 -0500

    Updated Player.h

--- Player/Player.h
+++ Player/Player.h
@@ -1,19 +1,28 @@
 #pragma once
 #include <string>
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Player {
 private:
-	string *territories;
-	string *cards;
-	string *orderlist;
+	string name;
+	vector<string*> territories;
+	vector<string*> cards;
+	vector<Order*> orderList;
 public:
 	Player();
-	Player(string name, string t[], string c[], string o[]);
+	Player(string name, vector<string*> t, vector<string*> h, vector<Order*>);
 	Player(const Player&);
+	~Player();
 	int toDefend();
 	int toAttack();
 	void issueOrder(string);
-	string *getOrderList();
+	vector<Order*> getOrderList();
+
+	//assignment operator
+	Player& operator=(const Player& player);
+
+	//stream insertion operator for Player
+	friend ostream& operator << (ostream& os, const Player& player);
 };
