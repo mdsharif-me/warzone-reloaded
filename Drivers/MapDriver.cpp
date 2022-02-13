0c947f5775910893b5c2cf42c162bd9ca4e08af5
Muhammad Sharif al-Islam <43485841+mdsharif-me@users.noreply.github.com>
2022-02-13 05:33:09 -0500

    Update MapDriver.cpp

---
+++ Drivers/MapDriver.cpp
@@ -0,0 +1,20 @@
+/*
+Author: Mohammad Shariful Islam
+Date: 12 Feb, 2022
+*/
+
+#include <string>
+#include "Map.h"
+
+int main() {
+	std::string mapFileName = "europass.map";
+	MapLoader map(mapFileName);
+	if (map.validateMap()) {
+		std::cout << "Creating map \"" << mapFileName << "\"...\n";
+		map.createMap();
+	}
+	else {
+		std::cout << "Error: Map file \"" << mapFileName << "\" is not a valid map.";
+	}
+	;
+}
