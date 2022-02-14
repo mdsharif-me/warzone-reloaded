ea4487d3ab830af56ce3438a0bf9c142ee27d127
chimirk <chirca.mircea@gmail.com>
2022-02-13 22:02:58 -0500

    first commit orders header Orders.h orders implementation Orders.cpp orders driver OrdersDriver.cpp merged player.h into main merged player .cpp into main merged player.driver into main intergrated ordersList into player implemented Cards.h implemented Cards.cpp implemented CardsDriver.cpp

--- Drivers/OrdersDriver.cpp
+++ Drivers/OrdersDriver.cpp
@@ -9,28 +9,64 @@ using namespace std;
 
 int main() {
 
-    auto* player = new Player();
-
+    auto * order = new Order();
     auto* deploy = new Deploy();
+    auto* advance = new Advance();
+    auto * bomb = new Bomb();
+    auto * blockade = new Blockade();
     auto* airlift = new Airlift();
-    Bomb* bomb = new Bomb();
+    auto * negotiate = new Negotiate();
 
+    cout << "adding orders to orderList...\n";
     auto* ordersList = new OrdersList();
+    ordersList->add(order);
     ordersList->add(deploy);
-    ordersList->add(airlift);
+    ordersList->add(advance);
     ordersList->add(bomb);
+    ordersList->add(blockade);
+    ordersList->add(airlift);
+    ordersList->add(negotiate);
 
+    cout << "displaying orders...\n";
     cout << *ordersList;
 
     //move first order from index 0 to index 2
+    cout << "moving order at index 0 to index 2\n";
     ordersList->move(0, 2);
 
-    cout << *ordersList;
+    cout << "displaying changes\n";
+    cout << *ordersList << endl;
+
+    //move first order from index 1 to index 3
+    cout << "moving order at index 1 to index 3\n";
+    ordersList->move(1, 3);
+    cout << "displaying changes\n";
+    cout << *ordersList << endl;
+
 
     // remove the order at position 0
+    cout << "removing order at index 0...\n";
     ordersList->remove(0);
-
+    cout << "displaying changes\n";
     cout << *ordersList;
+
+    // remove the order at position 3
+    cout << "removing order at index 3...\n";
+    ordersList->remove(3);
+    cout << "displaying changes\n";
+    cout << *ordersList;
+
+    cout << "validating negotiate order...\n";
+    if (negotiate->validate() == true) {}
+    cout << "order Negotiate is validated\n\n";
+
+    cout << "executing orders...\n";
+    deploy->execute();
+    advance->execute();
+    bomb->execute();
+    blockade->execute();
+    airlift->execute();
+    negotiate->execute();
 
     delete ordersList;
 
