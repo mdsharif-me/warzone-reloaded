223492bf1dc1ee329cba5f746d2928a6932c262b
Muhammad Sharif al-Islam <43485841+mdsharif-me@users.noreply.github.com>
2022-02-15 20:17:46 -0500

    Update Map.h

--- Headers/Map.h
+++ Headers/Map.h
@@ -10,25 +10,35 @@ Date: 12 Feb, 2022
 
 class Territory {
 public:
-    Territory(std::string territoryName);
-	Territory(std::string territoryName, std::string continentName, std::string edges, std::string playerName, int armyCount);
+	Territory();
+	~Territory();
+	Territory(std::string territoryName, std::string continentName, std::string playerName, int armyCount);
 	std::string getTerritoryName();
 	std::string getContinentName();
-	std::string getEdges();
 	std::string getPlayerName();
 	int getArmyCount();
 	void setTerritoryName(std::string territoryName);
 	void setContinentName(std::string continentName);
-	void setEdges(std::string edges);
 	void setPlayerName(std::string playerName);
 	void setArmyCount(int armyCount);
 
 private:
 	std::string territoryName;
 	std::string continentName;
-	std::string edges;
 	std::string playerName;
 	int armyCount;
+};
+
+class Map {
+public:
+	Map(int territoriesCount);
+	~Map();
+	std::list<int> getEdge(int index);
+	void setEdge(int edgeA, int edgeB);
+
+private:
+	int territoriesCount;
+	std::list<int>* map;
 };
 
 class MapLoader {
@@ -36,22 +46,16 @@ public:
 	MapLoader(std::string inputFileName);
 	bool validateMap();
 	void createTerritory();
-	int getNthSpace(std::string inputString, int index);
+	void buildMap();
+	std::string extractWord(std::string inputString, int index);
+	int extractInt(std::string inputString, int index);
+	std::vector<int> extractAllInt(std::string inputString);
 
 private:
 	std::string mapFileName;
 	int fieldCount;
 	std::vector<std::string> continents;
-	std::vector<std::string> territories;
-	std::vector<std::string> edges;
-};
-
-class Map {
-public:
-	Map(int territoriesCount);
-	void setTerritory(Territory objTerritory);
-
-private:
-	int territoriesCount;
-	std::vector<Territory> territoryObjects;
+	std::vector<std::string> countries;
+	std::vector<std::string> borders;
+	std::vector<Territory> territories;
 };
