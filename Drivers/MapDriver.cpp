c5df9a6ee0609ecec24a88697f15134e5af64761
Muhammad Sharif al-Islam <43485841+mdsharif-me@users.noreply.github.com>
2022-02-15 20:17:03 -0500

    Update MapDriver.cpp

--- Drivers/MapDriver.cpp
+++ Drivers/MapDriver.cpp
@@ -7,14 +7,25 @@ Date: 12 Feb, 2022
 #include "Map.h"
 
 int main() {
-	std::string mapFileName = "europass.map";
-	MapLoader map(mapFileName);
-	if (map.validateMap()) {
-		std::cout << "Creating map \"" << mapFileName << "\"...\n";
-		map.createTerritory();
+	std::cout << "------------------\n";
+	std::cout << "| MapLoader v1.0 |\n";
+	std::cout << "------------------\n";
+	std::cout << "\nThe map loader has started.\n" << std::endl;
+
+	std::vector<std::string> mapFileList = { "europass.map", "MyMap.map", "artic.map" };
+
+	// Check the map files and build maps
+	for (int i = 0; i < mapFileList.size(); i++) {
+		MapLoader map(mapFileList[i]);
+		if (map.validateMap()) {
+			map.buildMap();
+			std::cout << "\nSuccess: Map \"" << mapFileList[i] << "\" has been built.\n\n";
+		}
+		else {
+			std::cout << "\nError: Map file \"" << mapFileList[i] << "\" is invalid.\n\n";
+		}
 	}
-	else {
-		std::cout << "Error: Map file \"" << mapFileName << "\" is not a valid map.";
-	}
-	;
+
+	std::cout << "\n\n" << std::endl;
+	return 0;
 }
